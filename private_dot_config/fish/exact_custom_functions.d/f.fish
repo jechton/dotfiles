49aee6be3faf7f0bee490972dfc61b0f0b9400a9
Jeremiah <jechton@noreply.localhost>
2025-06-17 12:36:42 -0400

    feat(fish): add command correction

--- private_dot_config/fish/exact_custom_functions.d/f.fish
+++ private_dot_config/fish/exact_custom_functions.d/f.fish
@@ -1,3 +1,9 @@
-function f --description "Open current dir with file manager"
-    xdg-open ./ >/dev/null 2>&1 & disown
+function f -d "Correct your previous console command"
+  set -l fucked_up_command $history[1]
+  env TF_SHELL=fish TF_ALIAS=f PYTHONIOENCODING=utf-8 thefuck $fucked_up_command THEFUCK_ARGUMENT_PLACEHOLDER $argv | read -l unfucked_command
+  if [ "$unfucked_command" != "" ]
+    eval $unfucked_command
+    builtin history delete --exact --case-sensitive -- $fucked_up_command
+    builtin history merge
+  end
 end
