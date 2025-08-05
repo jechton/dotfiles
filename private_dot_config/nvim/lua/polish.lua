63f43df9ec89ce23a821f4a0144a79beacbdca9c
Jeremiah <jechton@noreply.localhost>
2025-08-04 23:19:47 -0400

    feat(nvim): update to astronvim v5

--- private_dot_config/nvim/lua/polish.lua
+++ private_dot_config/nvim/lua/polish.lua
@@ -1,25 +1,12 @@
--- This will run last in the setup process and is a good place to configure
--- things like custom filetypes. This is just pure lua so anything that doesn't
--- fit in the normal config locations above can go here.
-
--- Set up custom filetypes
--- vim.filetype.add({
--- 	extension = {
--- 		foo = "fooscript",
--- 	},
--- 	filename = {
--- 		["Foofile"] = "fooscript",
--- 	},
--- 	pattern = {
--- 		["~/%.config/foo/.*"] = "fooscript",
--- 	},
--- })
-
+-- This will run last in the setup process.
+-- This is just pure lua so anything that doesn't
+-- fit in the normal config locations above can go here
+--
 -- Reset cursor when exiting
 vim.api.nvim_create_augroup("cursorshape", { clear = true })
 vim.api.nvim_create_autocmd("VimLeave", {
-	desc = "Restore cursor shape on VimLeave",
-	command = "set guicursor=a:ver25-blinkon1",
-	pattern = "*",
-	group = "cursorshape",
+  desc = "Restore cursor shape on VimLeave",
+  command = "set guicursor=a:ver25-blinkon1",
+  pattern = "*",
+  group = "cursorshape",
 })
