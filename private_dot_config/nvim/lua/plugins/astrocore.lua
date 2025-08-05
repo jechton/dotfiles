e4250753f83560a2f134544bd9904d605d8f18b3
Jeremiah <44993244+jechton@users.noreply.github.com>
2025-08-04 23:19:47 -0400

    feat(nvim): update to astronvim v5

--- private_dot_config/nvim/lua/plugins/astrocore.lua
+++ private_dot_config/nvim/lua/plugins/astrocore.lua
@@ -1,5 +1,7 @@
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
+-- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
+--       as this provides autocomplete and documentation while editing
 
 ---@type LazySpec
 return {
@@ -11,7 +13,7 @@ return {
 			large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
 			autopairs = true, -- enable autopairs at start
 			cmp = true, -- enable completion at start
-			diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
+			diagnostics = { virtual_text = true, virtual_lines = false }, -- diagnostic settings on startup
 			highlighturl = true, -- highlight URLs at start
 			notifications = true, -- enable notifications at start
 		},
@@ -20,16 +22,28 @@ return {
 			virtual_text = true,
 			underline = true,
 		},
+		-- passed to `vim.filetype.add`
+		filetypes = {
+			-- see `:h vim.filetype.add` for usage
+			extension = {
+				foo = "fooscript",
+			},
+			filename = {
+				[".foorc"] = "fooscript",
+			},
+			pattern = {
+				[".*/etc/foo/.*"] = "fooscript",
+			},
+		},
 		-- vim options can be configured here
 		options = {
 			opt = { -- vim.opt.<key>
 				relativenumber = true, -- sets vim.opt.relativenumber
 				number = true, -- sets vim.opt.number
 				spell = false, -- sets vim.opt.spell
-				signcolumn = "auto", -- sets vim.opt.signcolumn to auto
+				signcolumn = "yes", -- sets vim.opt.signcolumn to yes
 				wrap = true, -- sets vim.opt.wrap
 				whichwrap = "h,l,<,>,[,]", -- move between lines at ends
-				scrolloff = 10,
 			},
 			g = { -- vim.g.<key>
 				-- configure global vim variables (vim.g)
